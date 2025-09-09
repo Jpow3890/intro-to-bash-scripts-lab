1db250ee9e72a48791a82cd25d643700e75dd46b
JamalP <Jamalpowell78@yahoo.com>
2025-09-09 10:47:14 -0400

    finished lab

---
+++ intro-to-python-for-scripting/intro_to_python.py
@@ -0,0 +1,124 @@
+# This is a simple print statement in Python
+
+"""
+This is a mulitli-line comment
+that explains that the following line of Code 
+prints "Hello, World!" to the console.
+
+We also call these DocStrings
+"""
+
+# String Data Types
+
+First_name = "Jamal" # single line string
+
+# Multi-line string
+multiline_string = """
+THis is a multi-line string
+this can span across multiple lines
+"""
+
+# print(type(First_name)) 
+
+# Number Data Types
+
+age = 30 # integer data type
+pi = 3.14 # float data type
+
+# print(type(age))
+
+
+# Boolean Data Types
+
+is_raining = True
+is_sunny = False
+
+# if is_raining:
+    # print("It's raining, get the umbrella!")
+# else:
+    # print("It's not raining, enjoy your day!")
+
+
+    
+    
+# Sequence Data Types
+
+
+# fruits = "apple, banana, cherry" # string is a sequence of character
+
+#print(fruits[0:5])
+
+fruits_list = ["apple", "banana" , "cherry"]
+
+fruits_list.append("orange")
+
+fruits_list.remove("banana")
+
+# print(fruits_list)
+
+Car = {
+    "make": "Honda",
+    "model": "Civic",
+    "year": 2009
+}
+  
+
+# print(car["make"])
+# print(car["num_doors", "key not found"])
+
+# print(type(car))
+
+# print("The Sum of 2 and 3 is: ", 2 + 3)
+
+#using the input method to retrieve user input
+# First_name = input("Please enter your first name: ")
+
+# print(f"Hello, {First_name}")
+
+a = 5
+b = 10
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+
+
+# print(a > b) # False
+# print(a < b) # True
+# print(a == b) # False
+# print(a != b) # True
+# print(a >= b) # False
+# print(a <= b) # True
+
+# print(a > b and a >= b) # False
+# print(a < b or a >= b) # True
+
+# age = int(input("Please enter your age: "))
+
+# if age > 20:
+    # print("you are old enough to drink alcohol in the US")
+
+# elif age < 21 and age < 18:
+    # print("you are not an adult in the US yet")
+
+# else: 
+    # print("you are not old enough to drink alcohol in the US")
+
+
+# Fruits = ["apple" , "banana" , "cherry"]
+
+# for fruit in Fruits:
+    # print(fruit)
+
+
+
+
+
+
+
+
+
+
+
+
